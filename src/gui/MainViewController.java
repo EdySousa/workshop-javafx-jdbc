d83677f91efc314c226fb2b5490d112405c4fc7d
EdySousa <edvaldo.segundo@hotmail.com>
2019-06-17 22:55:34 +0100

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -30,7 +39,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");	
+		loadView("/gui/About.fxml");
 	}
 
 	
@@ -38,6 +47,40 @@ public class MainViewController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		
 		
+	}
+	/*
+	 * O synchronized permite que todo processo correr sem ser interrompido
+	 * Isto a app grafica é uma aplicação multhread, varias a executar, inclusivo a thread da tela
+	 * */
+	
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+			//pegar a referencia para o VBox da cena principal (Main)
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			/*
+			 * O processo abaixo permite adicioinar a nova view na view principal
+			 * 1º guarda-se o primeiro filho do menu principal
+			 * 2º limpa-se todos os filhos do menu principal
+			 * 3º volta adicionar o primeiro filho do meunu principal
+			 * 4º por ultimo adicona-se os filhos daa nova view
+			 * Desta forma temos todos os filho na view principal*/
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	
