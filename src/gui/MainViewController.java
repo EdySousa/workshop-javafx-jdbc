54a87c777f2137273fbdc0e89fbe2c3238f4cb91
EdySousa <edvaldo.segundo@hotmail.com>
2019-06-20 10:49:31 +0100

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,75 +15,105 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
-	
+
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");	
+		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
+
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
 		loadView("/gui/About.fxml");
 	}
 
-	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
-		
+
 	}
 	/*
-	 * O synchronized permite que todo processo correr sem ser interrompido
-	 * Isto a app grafica é uma aplicação multhread, varias a executar, inclusivo a thread da tela
-	 * */
-	
+	 * O synchronized permite que todo processo correr sem ser interrompido Isto a
+	 * app grafica é uma aplicação multhread, varias a executar, inclusivo a thread
+	 * da tela
+	 */
+
 	private synchronized void loadView(String absoluteName) {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
-			
+
 			Scene mainScene = Main.getMainScene();
-			
-			//pegar a referencia para o VBox da cena principal (Main)
+
+			// pegar a referencia para o VBox da cena principal (Main)
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
+
 			/*
-			 * O processo abaixo permite adicioinar a nova view na view principal
-			 * 1º guarda-se o primeiro filho do menu principal
-			 * 2º limpa-se todos os filhos do menu principal
-			 * 3º volta adicionar o primeiro filho do meunu principal
-			 * 4º por ultimo adicona-se os filhos daa nova view
-			 * Desta forma temos todos os filho na view principal*/
-			
+			 * O processo abaixo permite adicioinar a nova view na view principal 1º
+			 * guarda-se o primeiro filho do menu principal 2º limpa-se todos os filhos do
+			 * menu principal 3º volta adicionar o primeiro filho do meunu principal 4º por
+			 * ultimo adicona-se os filhos daa nova view Desta forma temos todos os filho na
+			 * view principal
+			 */
+
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			
+
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
-	
-	
+	private synchronized void loadView2(String absoluteName) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+
+			// pegar a referencia para o VBox da cena principal (Main)
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			/*
+			 * O processo abaixo permite adicioinar a nova view na view principal 1º
+			 * guarda-se o primeiro filho do menu principal 2º limpa-se todos os filhos do
+			 * menu principal 3º volta adicionar o primeiro filho do meunu principal 4º por
+			 * ultimo adicona-se os filhos daa nova view Desta forma temos todos os filho na
+			 * view principal
+			 */
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
